eb19ceb359177b843aeb81035e471653ecbaa22b
404-GCross <lwhgcross@163.com>
2026-06-19 08:58:53 +0800

    refactor: 统一补丁和游戏下载临时目录到AppData

--- client/lib/services/steam_service.dart
+++ client/lib/services/steam_service.dart
@@ -10,7 +10,6 @@ import "package:http/http.dart" as http;
 import "package:path_provider/path_provider.dart";
 
 import "api_client.dart";
-import "download_service.dart";
 
 class SteamGameInfo {
   final String appId;
@@ -185,14 +184,8 @@ class SteamService {
       final total = resp.statusCode == 206
           ? (resp.contentLength ?? 0) + state._received
           : resp.contentLength ?? 0;
-      // PC: download to user's download dir; Android: use app support dir
-      String workDir;
-      if (Platform.isAndroid) {
-        workDir = (await getApplicationSupportDirectory()).path;
-      } else {
-        final downloadSvc = DownloadService();
-        workDir = await downloadSvc.downloadDir;
-      }
+      // Both platforms: temp files in app support dir (consistent with game downloads)
+      final workDir = (await getApplicationSupportDirectory()).path;
       if (tmpFile == null) {
         // Use ASCII-only temp name to avoid 7z encoding issues with CJK filenames
         final ext = patchFilename.contains(".") ? patchFilename.substring(patchFilename.lastIndexOf(".")) : "";
