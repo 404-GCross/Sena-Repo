66205210c5bd145770c121010ebdc590ef0725f5
404-GCross <lwhgcross@163.com>
2026-07-22 18:27:04 +0800

    fix: restore profile edit preferences import

--- client/lib/screens/profile_edit_screen.dart
+++ client/lib/screens/profile_edit_screen.dart
@@ -7,6 +7,7 @@ import "package:file_picker/file_picker.dart";
 import "package:flutter/material.dart";
 import "package:http/http.dart" as http;
 import "package:provider/provider.dart";
+import "package:shared_preferences/shared_preferences.dart";
 
 import "../providers/game_provider.dart";
 import "../services/secure_store.dart";
